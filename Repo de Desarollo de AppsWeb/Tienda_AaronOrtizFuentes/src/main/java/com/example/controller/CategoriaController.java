3d48119203aa4e76d3a3bda0836e6da34b5dcddf
AaronO <4ar0n0rt1zfu3nt3s@gmail.com>
2026-02-25 02:35:16 -0600

    .

--- Repo de Desarollo de AppsWeb/Tienda_AaronOrtizFuentes/src/main/java/com/example/controller/CategoriaController.java
+++ Repo de Desarollo de AppsWeb/Tienda_AaronOrtizFuentes/src/main/java/com/example/controller/CategoriaController.java
@@ -1,6 +1,13 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ @Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+
+    @GetMapping("/listado")
+    public String listado() {
+        return "categoria/listado";
+    }
+}
  */
 
 package com.example.controller;
